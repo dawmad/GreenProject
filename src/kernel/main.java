423230cf1dd0ecc308972b92f3b64da4fa08fd39
tomekniemczyk <tomekniemczyk@localhost>
2018-03-01 14:08:42 +0100

    Zmiana nazwy

--- src/kernel/main.java
+++ src/kernel/main.java
@@ -3,7 +3,7 @@
  * To change this template file, choose Tools | Templates
  * and open the template in the editor.
  */
-package javafxapplication1;
+package kernel;
 
 import javafx.application.Application;
 import javafx.fxml.FXMLLoader;
@@ -15,7 +15,7 @@ import javafx.stage.Stage;
  *
  * @author student
  */
-public class JavaFXApplication1 extends Application {
+public class main extends Application {
     
     @Override
     public void start(Stage stage) throws Exception {
